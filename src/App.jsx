7c5e3e3a64bf8a142394f3a480a95c2c97b48a04
Robert Schade <roscha80@web.de>
2021-05-11 09:22:49 +0200

    Fixed wrong code on Card Element

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,14 @@ import Card from './Card'
 
 export default () => {
   return (
-    <section>
-      <div>
-        <Header isBackButtonVisible>Header with Button</Header>
-        <Header>Another Header</Header>
-      </div>
-      <div>
-        <Card>Card</Card>
-      </div>
-      <div>
-        <Pagination isLeftDisabled text="1/34" />
-        <Pagination text="2/34" />
-        <Pagination isRightDisabled text="34/34" />
-        <Button isActive>Active</Button>
-        <Button isActive>Not active</Button>
-      </div>
-    </section>
+    <div>
+      <Header isBackButtonVisible>Header with Button</Header>
+      <Header>Another Header</Header>
+      <Card />
+      <Card isBookmarked />
+      <Pagination isLeftDisabled text="1/34" />
+      <Pagination text="2/34" />
+      <Pagination isRightDisabled text="34/34" />
+    </div>
   )
 }
