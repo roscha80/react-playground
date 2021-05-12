60a66e60b57317a093ed83a6aa860a82feb127b6
Robert Schade <roscha80@web.de>
2021-05-12 14:06:57 +0200

    Add todo, state array, input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,36 @@
-import * as React from 'react'
-import Pagination from './Pagination'
-import Header from './Header'
-import Card from './Card'
+import React, { useState } from 'react'
 
-const tags = [tag1, tag2, tag3]
+import { v4 as uuidv4 } from 'uuid'
 
-const cards = [
-  {
-    title: 'Is this the end?',
-    text:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos fugiat, libero totam sapiente vel culpa quod qui consequuntur nemo, similique facere sequi itaque corrupti quisquam, aut tempore debitis. Magni, iusto.',
-    isBookmarked: false,
-    isVisible: true,
-    id: 1,
-    tag1: 'tag1',
-    tag2: 'tag2',
-  },
-  {
-    title: 'The question is what is the question?',
-    text:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos fugiat, libero totam sapiente vel culpa quod qui consequuntur nemo, similique facere sequi itaque corrupti quisquam, aut tempore debitis. Magni, iusto.',
-    isBookmarked: true,
-    isVisible: false,
-    id: 2,
-    tag1: 'tag1',
-  },
-  {
-    title: 'Oh, really?',
-    text:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos fugiat, libero totam sapiente vel culpa quod qui consequuntur nemo, similique facere sequi itaque corrupti quisquam, aut tempore debitis. Magni, iusto.',
-    isBookmarked: false,
-    isVisible: true,
-    id: 3,
-    tag1: 'tag1',
-    tag2: 'tag2',
-    tag3: 'tag3',
-  },
-]
-
-export default function App() {
+export default () => {
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
-      <Header isBackButtonVisible>Header with Button</Header>
-      {cards.map(({ id, title, text, isBookmarked, isVisible, tags }) => (
-        <Card
-          key={id}
-          title={title}
-          text={text}
-          isBookmarked={isBookmarked}
-          isVisible={isVisible}
-          tags={tags}
-        />
-      ))}
-      <Pagination currentPage={1} totalPages={34} />
-      <Pagination currentPage={2} totalPages={34} />
-      <Pagination currentPage={34} totalPages={34} />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add todo:
+          <input name="todo" type="text" />
+        </label>
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map(({ text, isDone, id }) => (
+          <li key={id}>
+            {text}
+            {isDone && 'Done'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    //spread operator
+    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
+    setTodos([newTodo, ...todos])
+    form.reset()
+    input.focus()
+  }
 }
